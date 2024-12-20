1d7edd6489803453413059f042a7e93df907432d
Astrothunder001 <olorunfemidaniel98@gmail.com>
2024-12-20 01:03:51 -0800

    new changes

--- annotation/urls.py
+++ annotation/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home,save_annotation,get_projects,get_images
+from .views import home, save_annotation, get_projects, get_images
 
 urlpatterns = [
-    path('',home,name = 'annotation_tool'),
-    path ('save/',save_annotation,name = 'save_annotation'),
-    path('project/',get_projects,name='projects'),
-    path('image/<int:project_id>/',get_images,name='images'),
-    ]
+    path('', home, name='annotation_tool'),
+    path('annotation/', save_annotation, name='save_annotation'),  # Fixed URL for saving annotations
+    path('project/', get_projects, name='projects'),
+    path('image/<int:project_id>/', get_images, name='images'),
+]
--- labelbox/urls.py
+++ labelbox/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from annotation.views import home  # Import the home view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('api/',include('annotation.urls')),
+    path('api/', include('annotation.urls')),  # Include API URLs from annotation app
+    path('', home, name='home'),  # Root path for the home view
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
